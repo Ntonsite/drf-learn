9368ad66215812a105d1e0126d2e5a626f94bb16
Ntonsite Mwamlima <nmwamlima@accessbank.internal>
2020-03-24 12:22:40 +0300

    API view Updated

--- api_backend/urls.py
+++ api_backend/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import *
+from .views import article_list, article_detail
 
 urlpatterns = [
     path('api/list', article_list),
--- api_backend/views.py
+++ api_backend/views.py
@@ -22,32 +22,28 @@ def article_list(request):
 
 		if serializer.is_valid():
 			serializer.save()
-			return Response(serializer.data, satatus=status.HTTP_201_CREATED)
-		return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def article_detail(request, pk):
 	try:
 		article = Article.objects.get(pk=pk)
 
 	except Article.DoesNotExist:
-		return HttpResponse(satatus=404)
+		return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
 	if request.method == 'GET':
 		serializer = ArticleSerializer(article)
+		return Response(serializer.data)
 
-		return JsonResponse(serializer.data)
-
-	elif request.method == 'POST':
-		data = JSONParser().parse(request)
-		serializer = ArticleSerializer(article, data=data)
-
+	elif request.method == 'PUT':
+		serializer = ArticleSerializer(article, data=request.data)
 		if serializer.is_valid():
 			serializer.save()
-			return JsonResponse(serializer.data)
-
-		return JsonResponse(serializer.errors, satatus=400)
+			return Response(serializer.data)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 	elif request.method == 'DELETE':
 		article.delete()
-		return HttpResponse(satatus=204)
+		return Response(status=status.HTTP_204_NO_CONTENT)

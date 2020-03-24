c73e1ceb1786b1e1981ff5061b752226fb501fc7
Ntonsite Mwamlima <nmwamlima@accessbank.internal>
2020-03-24 12:50:50 +0300

    Classed Based View

--- api_backend/urls.py
+++ api_backend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import article_list, article_detail
+from .views import article_list, article_detail, ArticleAPIView
 
 urlpatterns = [
     path('api/list', article_list),
-    path('api/detail/<int:pk>/', article_detail)
+    path('api/article', ArticleAPIView.as_view()),
+    path('api/list/<int:pk>/', article_detail)
 ]
--- api_backend/views.py
+++ api_backend/views.py
@@ -2,13 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
-from .models import Article
+from .models import *
 from .serializers import *
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
-# Create your views here.
+# Class Based View
+class ArticleAPIView(APIView):
+	def get(self, request):
+		articles = Article.objects.all()
+		serializer = ArticleSerializer(articles, many=True)
+		return Response(serializer.data)
+	def post(self, request):
+		serializer = ArticleSerializer(data=request.data)
+
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
 
 @api_view(['GET', 'POST'])
 def article_list(request):
